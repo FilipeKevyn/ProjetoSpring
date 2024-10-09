ac53f0ab72ec5521722212aac62877afc82695db
Filipe Kevyn <FilipeKevyn4@gmail.com>
2024-10-09 13:45:46 -0300

    H2 database and JPA

--- src/main/java/com/springestudy/course/entites/User.java
+++ src/main/java/com/springestudy/course/entites/User.java
@@ -1,2 +1,81 @@
-package com.springestudy.course.entites;public class User {
+package com.springestudy.course.entites;
+
+import jakarta.persistence.*;
+
+import java.io.Serializable;
+import java.util.Objects;
+@Entity
+@Table(name = "tb_users")
+public class User implements Serializable {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+      private Long id;
+    private String name;
+    private String email;
+    private String phone;
+    private String pass;
+    public User(){
+
+    }
+
+    public User(Long id, String name, String email, String phone, String pass) {
+        this.id = id;
+        this.name = name;
+        this.email = email;
+        this.phone = phone;
+        this.pass = pass;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public String getPass() {
+        return pass;
+    }
+
+    public void setPass(String pass) {
+        this.pass = pass;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        User user = (User) o;
+        return Objects.equals(id, user.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
 }
--- src/main/java/com/springestudy/course/resources/UserResource.java
+++ src/main/java/com/springestudy/course/resources/UserResource.java
@@ -1,2 +1,18 @@
-package com.springestudy.course.resources;public class UserResource {
+package com.springestudy.course.resources;
+
+import com.springestudy.course.entites.User;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(value = "/users")
+public class UserResource {
+
+    @GetMapping
+    public ResponseEntity<User> findAll(){
+        User u = new User(1L, "Maria", "maria@gmail.com", "999999", "12345");
+        return ResponseEntity.ok().body(u);
+    }
 }
