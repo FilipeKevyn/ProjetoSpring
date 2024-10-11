a8af94c9bbc09836fbce3bdf9bed8c30c13b9827
Filipe Kevyn <FilipeKevyn4@gmail.com>
2024-10-11 19:20:13 -0300

    Enum

--- src/main/java/com/springestudy/course/entites/Order.java
+++ src/main/java/com/springestudy/course/entites/Order.java
@@ -1,6 +1,7 @@
 package com.springestudy.course.entites;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
+import com.springestudy.course.entites.enums.OrderStatus;
 import jakarta.persistence.*;
 
 import java.io.Serializable;
@@ -14,6 +15,8 @@ public class Order implements Serializable {
     private Long id;
     @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd'T'HH:mm:ss'Z'",timezone = "GMT")
     private Instant moment;
+
+    private Integer orderStatus;
     @ManyToOne
     @JoinColumn(name = "client_id")
     private User client;
@@ -22,9 +25,10 @@ public class Order implements Serializable {
 
     }
 
-    public Order(Long id, Instant moment, User client) {
+    public Order(Long id, Instant moment, OrderStatus orderStatus, User client) {
         this.id = id;
         this.moment = moment;
+        setOrderStatus(orderStatus);
         this.client = client;
     }
 
@@ -50,5 +54,15 @@ public class Order implements Serializable {
 
     public void setClient(User client) {
         this.client = client;
+    }
+
+    public OrderStatus getOrderStatus() {
+        return OrderStatus.valueOf(orderStatus);
+    }
+
+    public void setOrderStatus(OrderStatus orderStatus) {
+        if (orderStatus != null){
+            this.orderStatus = orderStatus.getCode();
+        }
     }
 }
